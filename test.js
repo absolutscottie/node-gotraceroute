a3c3af9e298cf9340e0f75b4235e19b2882449c3
Scott Lawson <scottie@Scotts-MacBook-Air.local>
2019-09-23 18:00:26 -0700

    added comments to test.js

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
+//include the ping module
 const ping = require("./build/Release/node-ping");
 
+//run the exported pingHost function, gather
+//results. quit when output indicates that the
+//trace is complete 
 for(var i = 1; i < 30; i++) {
   var resultStr = ping.pingHost("google.com", i, 2000, 1500);
 
